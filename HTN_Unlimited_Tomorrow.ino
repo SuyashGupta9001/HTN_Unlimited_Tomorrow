87e4877c5a6cd3bbb32862a599dfb17c55e00998
SuyashGupta9001 <136770799+SuyashGupta9001@users.noreply.github.com>
2023-06-16 09:38:48 +0530

    HTN_Unlimited_Tomorrow.ino

--- HTN_Unlimited_Tomorrow.ino
+++ HTN_Unlimited_Tomorrow.ino
@@ -18,18 +18,18 @@ int i = 10;
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //four columns
 //define the cymbols on the buttons of the keypads
-char hexaKeys[ROWS][COLS] = {
+  char hexaKeys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
   {'*','0','#'}
-};
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {10, 9, 8}; //connect to the column pinouts of the keypad
+  };
+  byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+  byte colPins[COLS] = {10, 9, 8}; //connect to the column pinouts of the keypad
 
-//initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
-char* password="4500";
+  //initialize an instance of class NewKeypad
+  Keypad CustomKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
 
 void setup()
 
@@ -40,17 +40,18 @@ void setup()
 	lcd.backlight();
   pinMode(7, OUTPUT);
   pinMode(buttonPin, INPUT);
+
 }
 
 void loop()
 { 
-
   lcd.setCursor(0, 0);
   lcd.print("Equipment ready for");
   lcd.setCursor(0, 1);
   lcd.print("Ammonia");
   delay(2000);
   lcd.clear();
+
 
   
   lcd.setCursor(0, 0);
@@ -86,21 +87,38 @@ void loop()
         lcd.setCursor(0, 1);
         lcd.print("pressure reading?");
         delay(500);
+        digitalWrite(7,HIGH);
+        delay(100);
 
-        if(buttonstate == HIGH){
+        if(buttonstate == 1){
 
-          digitalWrite(7, HIGH);
+          digitalWrite(7,LOW);
           char cus1;
-          cus1 = customKeypad.getKey(); 
-
-          if (cus1){
+          cus1 = CustomKeypad.getKey();
+          if (cus1 == "1"){
+            digitalWrite(7, HIGH);
+            lcd.clear();  
+            lcd.setCursor(0, 0);
+            lcd.print("Process 1 finished successfully");
+            lcd.clear();  
+            lcd.setCursor(0, 1);
+            lcd.print("Press any key to proceed to step 2");
+            if (cus1){
             Serial.println(cus1);
+            break;
             }
+
+          }
         }
       }
-    }
   
+  while(i>0){
+    lcd.clear();  
+    lcd.setCursor(0, 0);
+    lcd.print("Process 2 begining");
+    delay(100);
+  }
 
-
+}
 
 
